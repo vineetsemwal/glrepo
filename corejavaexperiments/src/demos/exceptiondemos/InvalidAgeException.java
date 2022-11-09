2d8a6ace3dfa2e5c69b0ae693fff1776bac26cdb
vineetsemwal <vineetsemwal82@gmail.com>
2022-11-09 12:35:37 +0530

    more exception demos

---
+++ corejavaexperiments/src/demos/exceptiondemos/InvalidAgeException.java
@@ -0,0 +1,7 @@
+package demos.exceptiondemos;
+
+public class InvalidAgeException extends Exception{
+    public InvalidAgeException(String msg){
+        super(msg);
+    }
+}
--- corejavaexperiments/src/demos/exceptiondemos/StringWork.java
+++ corejavaexperiments/src/demos/exceptiondemos/StringWork.java
@@ -2,25 +2,32 @@ package demos.exceptiondemos;
 
 public class StringWork {
 
-    public static void main(String args[]){
-            StringWork demo = new StringWork();
-            System.out.println("inside main before lowercase called");
-            try {
-                String result = demo.toLowerCase("good");
-                System.out.println("result=" + result);
-            }catch (InvalidArgumentException e){
-                System.out.println("invalid argument,"+e.getMessage());
-            }
+    public static void main(String args[]) {
+        StringWork demo = new StringWork();
+        System.out.println("inside main before lowercase called");
+        try {
+            String result = demo.toLowerCase("good", 70);
+            System.out.println("result=" + result);
+        } catch (InvalidArgumentException e) {
+            System.out.println("invalid argument," + e.getMessage());
+        }
+        catch (InvalidAgeException e){
+            System.out.println("age you rpovided is not accepted,"+e.getMessage());
+        }
 
     }
 
-    String toLowerCase(String input) throws InvalidArgumentException{
-            if (input == null || input.isEmpty()) {
-              throw  new InvalidArgumentException("input is null or empty");
-            }
-            String lowercased = input.toLowerCase();
-            System.out.println("after lowercase");
-            return lowercased;
+    String toLowerCase(String input, int age) throws InvalidArgumentException,InvalidAgeException {
+        if (input == null || input.isEmpty()) {
+            InvalidArgumentException e = new InvalidArgumentException("input is null or empty");
+            throw e;
+        }
+        if(age>60 || age<18){
+            throw new InvalidAgeException("age can't be greater thna 60");
+        }
+        String lowercased = input.toLowerCase();
+        System.out.println("after lowercase");
+        return lowercased;
 
     }
 
