34b1aaaca4db61b62254a41d633ddad219a7cf6b
vineetsemwal <vineetsemwal82@gmail.com>
2022-11-25 15:58:08 +0530

    basic mvc javaconfig demo

---
+++ jee/spring/mvc/mvcdemo/src/main/java/com/gltraining/mvcdemo/User.java
@@ -0,0 +1,44 @@
+package com.gltraining.mvcdemo;
+
+import java.util.Objects;
+
+public class User {
+    private int id;
+    private String name;
+
+    public User(){}
+
+    public User(int id, String name){
+        this.id=id;
+        this.name=name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
