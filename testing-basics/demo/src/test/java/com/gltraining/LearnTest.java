0ecdfea5f64b53e78b024ed9e27882632d7ec004
vineetsemwal <vineetsemwal82@gmail.com>
2022-12-16 10:19:52 +0530

    spring hackathon 2

--- testing-basics/demo/src/test/java/com/gltraining/LearnTest.java
+++ testing-basics/demo/src/test/java/com/gltraining/LearnTest.java
@@ -1,2 +1,21 @@
-package com.gltraining;public class LearnTest {
+package com.gltraining;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.List;
+
+public class LearnTest {
+
+    @Test
+    public void test1(){
+        List<Integer> list= Mockito.mock(List.class);
+        Mockito.when(list.get(0)).thenReturn(10);
+        Mockito.when(list.get(1)).thenReturn(20);
+
+        //using
+        int val1=list.get(0);
+        int val2=list.get(1);
+        System.out.println("val1="+val1+" ,val2="+val2);
+    }
 }
